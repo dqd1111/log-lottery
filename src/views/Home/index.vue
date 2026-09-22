3f922fd40e4b3e27f505527977d61c12950b5675
dqd1111 <2139777921@qq.com>
2026-09-22 14:57:46 +0800

    feat: add draw count control to home

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,13 +3,12 @@ import { storeToRefs } from 'pinia'
 import useStore from '@/store'
 import HeaderTitle from './components/HeaderTitle/index.vue'
 import OptionButton from './components/OptionsButton/index.vue'
-import PrizeList from './components/PrizeList/index.vue'
 import StarsBackground from './components/StarsBackground/index.vue'
 import { useViewModel } from './useViewModel'
 import 'vue-toast-notification/dist/theme-sugar.css'
 
 const viewModel = useViewModel()
-const { setDefaultPersonList, tableData, currentStatus, enterLottery, stopLottery, containerRef, startLottery, continueLottery, quitLottery, isInitialDone, titleFont, titleFontSyncGlobal } = viewModel
+const { setDefaultPersonList, tableData, currentStatus, enterLottery, stopLottery, containerRef, startLottery, continueLottery, quitLottery, isInitialDone, titleFont, titleFontSyncGlobal, drawCount, maxDrawCount, setDrawCount } = viewModel
 const globalConfig = useStore().globalConfig
 
 const { getTopTitle: topTitle, getTextColor: textColor, getTextSize: textSize, getBackground: homeBackground } = storeToRefs(globalConfig)
@@ -30,15 +29,17 @@ const { getTopTitle: topTitle, getTextColor: textColor, getTextSize: textSize, g
     <OptionButton
       :current-status="currentStatus"
       :table-data="tableData"
+      :draw-count="drawCount"
+      :max-draw-count="maxDrawCount"
       :enter-lottery="enterLottery"
       :start-lottery="startLottery"
       :stop-lottery="stopLottery"
       :continue-lottery="continueLottery"
       :quit-lottery="quitLottery"
+      :set-draw-count="setDrawCount"
     />
   </div>
   <StarsBackground :home-background="homeBackground" />
-  <PrizeList class="absolute left-0 top-32" />
 </template>
 
 <style scoped lang="scss">
